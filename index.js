3f57cc062c2b603a021051a44352779a0666b304
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-04-03 00:07:50 -0600

    arthimetic

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Write your code herelet multiply ;
+let multiply;
+let num1=2;
+let num2= 31;
+multiply=  num1 * num2;
+let random;
+random= Math.floor(Math.random() * 10) + 1;
+let mod;
+let num3=14;
+let num4=5;
+mod=num3 % num4;
+let max;
+max=Math.max(11, 12, 3, 20, 2);
